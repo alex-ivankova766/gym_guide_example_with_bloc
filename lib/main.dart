16e36801806359ed4ea661adf6113f20ecb65ab2
ivankova.alexandra766 <ivankova.alexandra766@gmail.com>
2024-04-04 09:14:54 +0500

    project created

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Gym Guide",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Gym Guide"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: 
+            Text(
+              'Development has begun',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+    ));
+  }
+}
